931352994492b5266754d4545902e86fd50d1fea
Nils Otto Johansen <nilsotto@gmail.com>
2018-10-23 22:12:04 +0000

    liste med meldinger

--- knapp.py
+++ knapp.py
@@ -12,9 +12,14 @@ infile_path = "/dev/input/event" + (sys.argv[1] if len(sys.argv) > 1 else "0")
 #long int, long int, unsigned short, unsigned short, unsigned int
 FORMAT = 'llHHI'
 EVENT_SIZE = struct.calcsize(FORMAT)
-#xml_string="<sms><til>+4790011048</til><melding>dette er en python-test</melding></sms>"
-#url="https://varsling-tjenester-felles-develop.cluster.dev/varsle/sms"
-#smses=[ "Nå tenker noen i Husbanken på deg", "Lurer du på om deploy 2.0 er i prod?", "3d-printeren ser trist ut i dag", "ping - http://husbanken.no", "Mjøndalen - Kjempers hjemsted", "" ] 
+xml_string="<sms><til>+4790011048</til><melding>dette er en python-test</melding></sms>"
+url="https://varsling-tjenester-felles-develop.cluster.dev/varsle/sms"
+smses=[ "Nå tenker noen i Husbanken på deg", 
+        "Lurer du på om deploy 2.0 er i prod?",
+        "3d-printeren ser trist ut i dag",
+        "ping - http://husbanken.no",
+        "Mjøndalen - Kjempers hjemsted",
+        "" ] 
 
 #open file in binary mode
 in_file = open(infile_path, "rb")
@@ -30,8 +35,10 @@ while event:
         now = datetime.datetime.now()
         delta = now - lastTime
         if (code == 4 and delta.seconds > 3):
-            number=delta.seconds % 8
-            print("SENDER SMS!!!!" + number
+            number=(delta.seconds % len(smses))
+            print("SENDER SMS!!!! %d" % number)
+            print(datetime.datetime.now())
+            print(smses[number])
             lastTime = datetime.datetime.now()
             #requests.post(url, data=xml_string, headers={'Content-Type':'application/xml; charset=UTF-8'},verify=False)
 
